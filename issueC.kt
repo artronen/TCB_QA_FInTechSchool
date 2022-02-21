2af32d9ad911845e0a7df42c0fef8b63da1ca867
Arthur Mironienko <art.ronen1@gmail.com>
2022-02-21 21:09:24 +0300

    fix

--- issueC.kt
+++ issueC.kt
@@ -4,8 +4,8 @@ fun checkIsScammer(name: String, surname : String): Boolean {
     if (name.length != surname.length) {
         return false
     }
-    val nameAsSet = name.toSet()
-    val surnameAsSet = surname.toSet()
+    val nameAsSet = name.toList()
+    val surnameAsSet = surname.toList()
     if (nameAsSet == surnameAsSet) {
         return true
     }
@@ -14,12 +14,12 @@ fun checkIsScammer(name: String, surname : String): Boolean {
 
 fun main () {
     val reader = Scanner(System.`in`)
-    print("Введите обфусцированный код: ")
+    print("имя и фамилию жулика: ")
     val name: String = reader.next().uppercase()
     val surname : String = reader.next().uppercase()
     print(when(checkIsScammer(name, surname)) {
         true -> "Yes"
         false -> "No"
     }
-        )
+    )
 }
